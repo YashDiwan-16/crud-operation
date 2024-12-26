9ab9759287efafddd4330718b217b5758b71dd28
YashTheCoder <yashmdiwan@gmail.com>
2024-12-26 22:39:42 +0530

    feat: implement CRUD operations for topics using MongoDB

--- app/api/topics/route.ts
+++ app/api/topics/route.ts
@@ -1,11 +1,35 @@
-import Topic from "@/app/models/topic";
-import connectMongoDB from "@/lib/mongodb";
-import { NextResponse } from "next/server";
+
+import clientPromise from "@/lib/clientpromise";
+
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: Request)
 {
-    const {task}= await request.json();
-    await connectMongoDB();
-       await Topic.create({task});
-       return NextResponse.json({message:'Task created successfully'}); 
+    // const {task}= await request.json();
+    // await connectMongoDB();
+    //    await Topic.create({task});
+    //    return NextResponse.json({message:'Task created successfully'}); 
+
+    const {task}=await request.json();
+    const client = await clientPromise;
+    const db = client.db("todo");
+    const topics = db.collection("topics");
+
+    const result =await topics.insertOne({task});
+    return NextResponse.json({message:'Task created successfully'});
+
+    
+
 }   
+
+
+export async function GET()
+{
+    const client = await clientPromise;
+    const db = client.db("todo");
+    const topics = db.collection("topics");
+
+    const result = await topics.find({}).toArray();
+    return NextResponse.json(result);
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { TodoList } from "@/components/todo-list";
-
+import React from "react";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-100 to-blue-100 dark:from-gray-900 dark:to-gray-800 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
